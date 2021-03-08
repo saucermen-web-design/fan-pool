670435f00c8e41298a31ca7890d087074c796bd2
cotymckinney <saucermenwebdesign@gmail.com>
2021-03-08 13:38:12 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -89,30 +89,3 @@ $(function() {
     };
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $('#pagination-demo').twbsPagination({
-//     totalPages: 35,
-//     visiblePages: 7,
-//     onPageClick: function (event, page) {
-//         $('#page-content').text('Page ' + page);
-//     }
-// });
